90ad0312c35664b9b3bfc22996109a70e229eb7f
Plipus Telaumbanua <plipus.tel@gmail.com>
2023-10-14 18:43:24 +0700

    App is udpated

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@
  * @Created Date: Mei 28, 2023
  * @Lastest Updated: Jun 01 2023
  * Test update
-*/
-import React, { useState, StyleSheet } from 'react';
-import './App.css'
+ */
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   /* Unseroted Data */
@@ -16,7 +16,7 @@ function App() {
 
   /* Bubble Sort Data Array */
   const [bubbleSortData, setBubbleSortData] = useState([...unsortedData]);
-  
+
   /* Selection Sort */
   const [selectionSortData, setSelectionSortData] = useState([...unsortedData]);
 
@@ -24,15 +24,15 @@ function App() {
     setMultipleTwo([...unsortedData]);
     setBubbleSortData([...unsortedData]);
     setSelectionSortData([...unsortedData]);
-  }
+  };
 
   const MultipleByTwo = () => {
     let arr = [];
-    for(let i = 0; i <= multipleTwo.length - 1; i++) {
+    for (let i = 0; i <= multipleTwo.length - 1; i++) {
       arr.push(multipleTwo[i] * 2);
     }
-    setMultipleTwo(arr)
-  }
+    setMultipleTwo(arr);
+  };
 
   /* 1. Bubble Sort 
   > Bubble sort is a simple sorting algorithm that repeatedly steps through a list of elements, 
@@ -49,9 +49,9 @@ function App() {
   const BubbleSort = () => {
     const newSortedData = [...bubbleSortData];
 
-    for(let i = 0; i <= newSortedData.length - 1; i++){
-      for( let j = 0; j <= newSortedData.length - i - 1; j++) {
-        if(newSortedData[j] > newSortedData[j + 1]) {
+    for (let i = 0; i <= newSortedData.length - 1; i++) {
+      for (let j = 0; j <= newSortedData.length - i - 1; j++) {
+        if (newSortedData[j] > newSortedData[j + 1]) {
           let temp = newSortedData[j]; //hold for temporary the bigger data, to switch its position later
           newSortedData[j] = newSortedData[j + 1];
           newSortedData[j + 1] = temp;
@@ -59,7 +59,7 @@ function App() {
       }
     }
     setBubbleSortData(newSortedData);
-  }
+  };
 
   /* 2. Selection Sort 
   > The selection sort algorithm always selects the smallest (or largest) element from the remaining unsorted 
@@ -67,56 +67,65 @@ function App() {
     the entire list is sorted. 
   > 
   */
-  
+
   const SelectionSort = () => {
     const newSortedData = [...bubbleSortData];
-    for(let i = 0; i <= newSortedData.length - 1; i++){
+    for (let i = 0; i <= newSortedData.length - 1; i++) {
       let minIndex = i; // assume i = 0 -> 4
-      for(let j = i; j <= newSortedData.length - 1; j++) { // we start from the next 'box'
-        if(newSortedData[j] < newSortedData[minIndex]) {
+      for (let j = i; j <= newSortedData.length - 1; j++) {
+        // we start from the next 'box'
+        if (newSortedData[j] < newSortedData[minIndex]) {
           minIndex = j; // assume found j = 3 -> 1 after j iteration
         }
       }
       // we need to move the founded minimum value to iteration i, and move i to founded j
-      console.log('iteration-' + i + ' minIndex found in ' + minIndex)
+      console.log("iteration-" + i + " minIndex found in " + minIndex);
       let temp = newSortedData[i];
       newSortedData[i] = newSortedData[minIndex];
       newSortedData[minIndex] = temp;
     }
     setSelectionSortData(newSortedData);
-  }
+  };
 
   return (
     <div>
       <h3>Unsorted Data</h3>
-      <ul className='numbers'>
+      <ul className="numbers">
         {unsortedData.map((rows, index) => (
-          <li className='items' key={index}>{rows}</li>
+          <li className="items" key={index}>
+            {rows}
+          </li>
         ))}
       </ul>
-      
+
       <h3>1.Multiple by Two</h3>
       <button onClick={MultipleByTwo}>Multiple by Two (arr * 2)</button>
       <button onClick={ResetNumbers}>Reset All Numbers</button>
-      <ul className='numbers'>
+      <ul className="numbers">
         {multipleTwo.map((rows, index) => (
-          <li className='items' key={index}>{rows}</li>
+          <li className="items" key={index}>
+            {rows}
+          </li>
         ))}
       </ul>
 
       <h3>2. Bubble Sort</h3>
       <button onClick={BubbleSort}>Bubble Sort</button>
-      <ul className='numbers'>
+      <ul className="numbers">
         {bubbleSortData.map((rows, index) => (
-          <li className='items' key={index}>{rows}</li>
+          <li className="items" key={index}>
+            {rows}
+          </li>
         ))}
       </ul>
 
       <h3>2. Selection Sort</h3>
       <button onClick={SelectionSort}>Selection Sort</button>
-      <ul className='numbers'>
+      <ul className="numbers">
         {selectionSortData.map((rows, index) => (
-          <li className='items' key={index}>{rows}</li>
+          <li className="items" key={index}>
+            {rows}
+          </li>
         ))}
       </ul>
     </div>
